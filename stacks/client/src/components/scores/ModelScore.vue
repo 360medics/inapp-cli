21121de2f40b3683eaa4e6c3eed814479a6706ec
AmelieChevalier <ameliechevalier3006@gmail.com>
2021-11-29 10:56:36 +0100

    apply eslint config on all files

--- stacks/client/src/components/scores/ModelScore.vue
+++ stacks/client/src/components/scores/ModelScore.vue
@@ -1,26 +1,26 @@
 <template>
-	<div class="modelScore">
-		<h3>Hello World</h3>
-	</div>
+    <div class="modelScore">
+        <h3>Hello World</h3>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from 'vue';
-import { useRoute } from 'vue-router';
-import { useScoreStore } from '@/store';
+import { defineComponent, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import { useScoreStore } from '@/store'
 
 export default defineComponent({
-	name: 'ModelScore',
-	setup() {
-		const route = useRoute();
-		const { getScore, score } = useScoreStore();
-		onMounted(() => {
-			getScore(route.params.slug);
-		});
+    name: 'ModelScore',
+    setup() {
+        const route = useRoute()
+        const { getScore, score } = useScoreStore()
+        onMounted(() => {
+            getScore(route.params.slug)
+        })
 
-		return {
-			score,
-		};
-	},
-});
+        return {
+            score,
+        }
+    },
+})
 </script>
