42032fd224479dc990c3b2c00f9e396151579d90
Romain Billot <romain.billot@360medics.com>
2021-12-10 13:35:09 +0100

    feat(client): vuejs build output filename hashing

--- stacks/client/vue.config.js
+++ stacks/client/vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
+    filenameHashing: true,
     transpileDependencies: true,
     publicPath: '',
     css: {
