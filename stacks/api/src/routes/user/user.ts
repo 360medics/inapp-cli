9923ec9c0a5ea3f01065fceb98f9ab3f4a3c198d
AmelieChevalier <ameliechevalier3006@gmail.com>
2021-12-13 15:51:10 +0100

    user:crud creation of routes create / update / delete

--- stacks/api/src/routes/user/user.ts
+++ stacks/api/src/routes/user/user.ts
@@ -11,3 +11,49 @@ export const getUser: RequestHandler = async (req, res) => {
 
   return res.status(200).json({ user });
 };
+
+export const createUser: RequestHandler = async (req, res) => {
+  if (!req.body) {
+    return res.status(404).json({ message: 'Body can not be empty' });
+  }
+
+  const user = await orm.user.create({ data: req.body });
+
+  return res.status(200).json(user);
+};
+
+export const updateUser: RequestHandler = async (req, res) => {
+  const { userId } = req.params;
+  const user = await orm.user.findUnique({ where: { id: userId } });
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  const updatedUser = await orm.user.update({
+    where: {
+      id: userId,
+    },
+    data: req.body,
+  });
+
+  return res.status(200).json(updatedUser);
+};
+
+export const deleteUser: RequestHandler = async (req, res) => {
+  const { userId } = req.params;
+  const user = await orm.user.findUnique({ where: { id: userId } });
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  const deletedUser = await orm.user.delete({
+    where: {
+      id: userId,
+    },
+  });
+  console.log(deletedUser);
+
+  return res.status(200).json({ message: 'User successfully deleted' });
+};
