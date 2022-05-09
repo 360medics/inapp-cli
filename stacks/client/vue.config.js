9fe47870a24694e62efa223f2fbc8d04676bfbd5
Romain Billot <romain.billot@360medics.com>
2022-05-09 16:23:34 +0200

    chore(client): add cors to public assets on dev mode

--- stacks/client/vue.config.js
+++ stacks/client/vue.config.js
@@ -9,6 +9,11 @@ module.exports = defineConfig({
             webSocketURL: "ws://0.0.0.0:3000/ws",
         },
         host: "0.0.0.0",
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+        },
     },
     css: {
         loaderOptions: {
